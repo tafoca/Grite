535c217dbf9e426ae4d6d10212fd9d6783697251
tafoca <devtafo@gmail.com>
2017-01-11 11:39:18 +0100

    fix bug else generate item of lower case, change type int to boolean

--- Grite.java
+++ Grite.java
@@ -5,9 +5,11 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
+
 /**
  * 
  * @author 
@@ -24,16 +26,18 @@ public class Grite {
 	private int nbtransaction = 9;
 	/** the list of current itemsets */
 	public ArrayList<float[]> itemsets = new ArrayList<>();
+	//static Hashtable<String, int[][]> allContengent = new Hashtable<>();
 	/** the name of the transcation file */
 	private String transaFile;
-	static ArrayList<int[][]> allContengent= new ArrayList<>();
+	static ArrayList<boolean[][]> allContengent= new ArrayList<>();
 	float[][] dataset;
 	float[] item;
 	int taille= 9;
 	int a = 3;
-	private String transafile = "transa.dat"; // default transaction file
-	private String configfile = "config.dat"; // default configuration file
-
+	private String transafile = "transa1.dat"; // default transaction file
+	private String configfile = "config1.dat"; // default configuration file
+	private static String[] attrList;
+	private static ArrayList<String> semantique = new ArrayList<>();
 	/*
 	 * 1. Génération des 1-itemsets graduels : pour chaque item i de la base DB,
 	 * l’item graduel i ≥ est construit en ordonnant les tO [i] selon la
@@ -50,9 +54,10 @@ public class Grite {
 		this.dataset = Grite.duplique(itemsets);
 		//Grite.affiche(dataset);
 		allContengent = createGradualsItemsetsOfSize1(dataset, item, a, taille);
-		Grite.affiche(allContengent.get(0));
+		//Grite.affiche(allContengent.get(0));
 		grite_execution();
-		
+		System.out.println("--------- ---- Grite.Grite()----------");
+		Grite.affiche(Grite.jointure(allContengent.get(0), allContengent.get(2)));
 		
 	}
 	
@@ -78,10 +83,27 @@ public class Grite {
 			// number of items
 			oneLine = data_in.readLine();
 			nbitems = Integer.valueOf(oneLine).intValue();
+			attrList = new String[nbitems];
+			//list of attribut
+			oneLine = data_in.readLine();
+			StringTokenizer listAttr = new StringTokenizer(oneLine, " "); 
+			String val;
+			int index = 0;
+			while (listAttr.hasMoreElements()) {
+				Object object = (Object) listAttr.nextElement();
+				val = (String) object;
+				attrList[index] = val;
+				index ++;
+			}
 			
 			// output configuration of the user
 			System.out.print("\nInput configuration: " + nbitems + " items,and  " + nbtransaction + " transactions. ");
+			System.out.print("\n Liste of Attribut: " + attrList[0] + " ,and  " + attrList[1]  + " ,and "+ attrList[2] );
 			System.out.println();
+			System.out.println();
+			for (int i = 0; i < semantique.size(); i++) {
+				System.out.println(semantique.get(i)+"  ");
+			}
 		} catch (IOException e) {
 			System.out.println(e);
 		}
@@ -127,11 +149,11 @@ public class Grite {
 		return res;
 	}
 
-	public static void affiche(int[][] tab) {
-		for (int[] d : tab) {
+	public static void affiche(boolean[][] tab) {
+		for (boolean[] d : tab) {
 			System.out.println();
-			for (int v : d)
-				System.out.print(v + " ");
+			for (boolean v : d)
+				System.out.print(" "+ (v ? 1 : 0 )+" ");
 		}
 	}
 
@@ -173,45 +195,58 @@ public class Grite {
 		}
 	}
 
-	private static ArrayList<int[][]> createGradualsItemsetsOfSize1( float[][] dataset,  float[] item, int a,
+	private  ArrayList<boolean[][]>  createGradualsItemsetsOfSize1( float[][] dataset,  float[] item, int a,
 			 int taille) {
-		ArrayList<int[][]> allContengent = new ArrayList<>();
+		ArrayList<boolean[][]> allContengent = new ArrayList<>() ;
 		for (int i = 0; i < 3; i++) {
 			float[] rescol = Grite.getDataColByCol(dataset, item, i, taille);
-
+			 String attr ="plus " +attrList[i] +"augmente";
 			// gestion objets croissant X> et creation matrice contigence
 			// associe
-			int[][] Contengence1 = new int[taille][taille];
+			boolean[][] Contengence1 = new boolean[taille][taille];
 
 			for (int j = 0; j < taille; j++) {
 				for (int j2 = j + 1; j2 < taille; j2++) {
-					if (rescol[j] < rescol[j2]) {
-						Contengence1[j][j2] = 1;
+					if (rescol[j] <= rescol[j2]) {
+						Contengence1[j][j2] = true;
 					}else{
-					Contengence1[j][j2] = 0;
+					Contengence1[j][j2] = false;
 					}
 				}
 			}
 			allContengent.add(Contengence1);
+			semantique.add(attr);
 			// gestion objets decroissant X< et creation matrice contigence
 			// associe
-
-			int[][] Contengence2 = new int[taille][taille];
+			attr ="plus " +attrList[i] +"diminue";
+			boolean[][] Contengence2 = new boolean[taille][taille];
 
 			for (int j = 0; j < taille; j++) {
 				for (int j2 = j + 1; j2 < taille; j2++) {
-					if (rescol[j] > rescol[j2]) {
-						Contengence2[j][j2] = 1;
+					if (rescol[j] >= rescol[j2]) {
+						Contengence2[j][j2] = true;
+					}else
+					{
+					Contengence2[j][j2] = false;
 					}
-					Contengence2[j][j2] = 0;
 				}
 			}
 			allContengent.add(Contengence2);
+			semantique.add(attr);
 		}
 		return allContengent;
 
 	}
-
+	public static  boolean[][]jointure(boolean[][] m1,boolean[][] m2 ){
+		boolean[][] res =new boolean[m1.length][m1.length];
+		
+		for (int i = 0; i < res.length; i++) {
+			for (int j = 0; j < res.length; j++) {
+				res[i][j] = (m1[i][j]&m2[i][j]);
+			}
+		}
+		return res;
+	}
 	public void grite_execution() {
 		// createGradualsItemsetsOfSize1();
 	}
@@ -228,20 +263,21 @@ public class Grite {
 		 * : arrayList) { System.out.println("< "+integer+ " />"); } }
 		 */
 		float[][] dataset = ap.dataset;//Grite.duplique(itemsets);
-		Grite.affiche(ap.dataset);
+		//Grite.affiche(ap.dataset);
 		System.out.println();
 		int a = 0;
 		//float[] item = null;
 		int taille = 9;
 		Grite.getAllColum(dataset, item, a, taille);
-		ArrayList<int[][]> allContengent =ap.createGradualsItemsetsOfSize1(dataset, item, 3, taille);
+		ArrayList<boolean[][]>  allContengent =ap.createGradualsItemsetsOfSize1(dataset, item, 3, taille);
 		//ap.createGradualsItemsetsOfSize1(ap.dataset, item, 3,taille);
-		System.out.println("Grite.main() " + ap.allContengent.size());
+		System.out.println("Grite.main() " + ap.allContengent.size()+ " nombre de regle graduel semantique :"+semantique.size());
 		//ArrayList<int[][]> allContengent = ap.allContengent;
 		System.out.println();
-		for (@SuppressWarnings("rawtypes")
-		Iterator iterator = allContengent.iterator(); iterator.hasNext();) {
-			int[][] is = (int[][]) iterator.next();
+		for (
+		Iterator<boolean[][]> iterator = (allContengent).iterator(); iterator.hasNext();) {
+			boolean[][] is = (boolean[][]) iterator.next();
+			//System.out.println("---------s1");
 			affiche(is);
 			System.out.println();
 			System.out.println("-----------------");
