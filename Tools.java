7d90bddb0cb4a123c501c9177ff486e75db89546
tafoca <devtafo@gmail.com>
2017-06-24 21:53:30 +0100

    add new tools method for test the generation of max gaduals pattern : fusion, testitem ,genIndexUsed

--- Tools.java
+++ Tools.java
@@ -1,5 +1,9 @@
 package uds.Grite;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 /**
  * @author tabueu Fotso Laurent
  * @copyright GNU General Public License v3 No reproduction in whole or part
@@ -8,8 +12,76 @@ package uds.Grite;
  */
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.StringTokenizer;
 
 public class Tools {
+
+	/**
+	 * @category configuration property file
+	 */
+	int itemNembers;
+	/**
+	 * @category configuration property file
+	 */
+	int nbTransaction;
+	String filename;
+
+	public int getItemNembers() {
+		return itemNembers;
+	}
+
+	public void setItemNembers(int itemNembers) {
+		this.itemNembers = itemNembers;
+	}
+
+	public int getNbTransaction() {
+		return nbTransaction;
+	}
+
+	public void setNbTransaction(int nbTransaction) {
+		this.nbTransaction = nbTransaction;
+	}
+
+	public void initParameter(String filename) {
+		int countTrans = 0;
+		int countAttr = 0;
+		boolean drapeau = true;
+		BufferedReader data_in;
+		try {
+			data_in = new BufferedReader(new FileReader(filename));
+			try {
+				while (data_in.ready()) {
+					String line = data_in.readLine();
+					if (line.matches("\\s*"))
+						continue; // be friendly with empty lines
+					countTrans++;
+					if (drapeau) {
+						StringTokenizer t = new StringTokenizer(line, " ");
+						countAttr = t.countTokens();
+						drapeau = false;
+					}
+
+				}
+				setItemNembers(countAttr);
+				setNbTransaction(countTrans);
+				toString();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public String toString() {
+		return "Config [itemNembers=" + itemNembers + ", nbTransaction=" + nbTransaction + ", filename=" + filename
+				+ "]";
+	}
+
 	int i;
 	int[] memory;
 	int sizeMat = 10;
@@ -185,10 +257,10 @@ public class Tools {
 
 	// computation of support of all contengence matrix boolmatrix
 	public int maximumSupport(boolean[][] boolMatrix, String[] pattern, int[] memory) { // pattern,
-																						// memory,
+		// memory,
 		int max = sizeMaxWay(boolMatrix, (int) getRoots(boolMatrix).get(0));
 		for (int i = 0; i < getRoots(boolMatrix).size(); i++) {// , pattern,
-																// memory,
+			// memory,
 			int tmp = sizeMaxWay(boolMatrix, i);
 			if (max < tmp) {
 				max = tmp;
@@ -227,12 +299,401 @@ public class Tools {
 		}
 		return attrList;
 	}
-	public void writeFileOut(ArrayList<String[]> semantique){
-		
+
+	public void writeFileOut(ArrayList<String[]> semantique) {
+
 		for (Iterator iterator = semantique.iterator(); iterator.hasNext();) {
 			String[] strings = (String[]) iterator.next();
-			
+
 		}
-		
+
 	}
+
+	public ArrayList<String[]> genIndexUsed(ArrayList<String[]> unitem, ArrayList<String[]> unitemFreq) {
+		int n1 = unitem.size(), n2 = unitemFreq.size();
+		ArrayList<String[]> valrtn = new ArrayList<>();
+		System.out.println(n1 + "  --  " + n2);
+		switch (n1) {
+		case 4:
+			for (int i = 0; i <= 1; i++) {
+				for (int j = 2; j < 4; j++) {
+					// condition unitem[i] et unitem[j] arptienne a unitemFreq
+					// ==>true sinon false
+					String[] tm = unitem.get(i);
+					String tmst1 = tm[0];
+					String tmst2 = unitem.get(j)[0];
+					if (!(tmst1 == tmst2)) {
+						valrtn.add(lexicalFusion(unitem.get(i), unitem.get(j)));
+					}
+				}
+			}
+			break;
+
+		case 6:
+			for (int i = 0; i < 2; i++) {
+				for (int j = 2; j < 4; j++) {
+					for (int k = 4; k < 6; k++) {
+						// condition unitem[i] et unitem[j] arptienne a
+						// unitemFreq
+						// ==>true sinon false
+						String[] tm = unitem.get(i);
+						String tmst1 = tm[0];
+						String tmst2 = unitem.get(j)[0];
+						String tmst3 = unitem.get(k)[0];
+
+						if (testitem(tmst1, tmst2, tmst3)) {
+							String[] val = lexicalFusion(unitem.get(i), unitem.get(j));
+							String[] val3 = unitem.get(k);
+							String[] add = Fusion(val, val3);
+							valrtn.add(add);
+						}
+					}
+
+				}
+			}
+			break;
+
+		case 8:
+			for (int i = 0; i < 2; i++) {
+				for (int j = 2; j < 4; j++) {
+					for (int k = 4; k < 6; k++) {
+						for (int l = 6; l < 8; l++) {
+							String tmst1 = unitem.get(i)[0];
+							String tmst2 = unitem.get(j)[0];
+							String tmst3 = unitem.get(k)[0];
+							String tmst4 = unitem.get(l)[0];
+							boolean b = testitem(tmst1, tmst2, tmst3, tmst4);
+							if (b) {
+								String[] add_f, val = lexicalFusion(unitem.get(i), unitem.get(j));
+								String[] val3 = unitem.get(k);
+								String[] val4 = unitem.get(l);
+								String[] add = Fusion(val, val3);
+								add_f = Fusion(add, val4);
+								valrtn.add(add_f);
+							}
+						}
+					}
+
+				}
+			}
+			break;
+
+		case 10:
+			for (int i = 0; i < 2; i++) {
+				for (int j = 2; j < 4; j++) {
+					for (int k = 4; k < 6; k++) {
+						for (int l = 6; l < 8; l++) {
+							for (int l2 = 8; l2 < 10; l2++) {
+								String tmst1 = unitem.get(i)[0];
+								String tmst2 = unitem.get(j)[0];
+								String tmst3 = unitem.get(k)[0];
+								String tmst4 = unitem.get(l)[0];
+								String tmst5 = unitem.get(l2)[0];
+								boolean b = testitem(tmst1, tmst2, tmst3, tmst4, tmst5);
+								if (b) {
+									String[] add_f, add_f1, val = lexicalFusion(unitem.get(i), unitem.get(j));
+									String[] val3 = unitem.get(k);
+									String[] val4 = unitem.get(l);
+									String[] val5 = unitem.get(l2);
+									String[] add = Fusion(val, val3);
+									add_f = Fusion(add, val4);
+									add_f1 = Fusion(add_f, val5);
+									valrtn.add(add_f1);
+								}
+							}
+						}
+					}
+
+				}
+			}
+			break;
+
+		case 12:
+			for (int i = 0; i < 2; i++) {
+				for (int j = 2; j < 4; j++) {
+					for (int k = 4; k < 6; k++) {
+						for (int l = 6; l < 8; l++) {
+							for (int l2 = 8; l2 < 10; l2++) {
+								for (int m = 10; m < 12; m++) {
+									/*
+									 * String tmst1 = unitem.get(i)[0]; String
+									 * tmst2 = unitem.get(j)[0]; String tmst3 =
+									 * unitem.get(k)[0]; String tmst4 =
+									 * unitem.get(l)[0]; String tmst5 =
+									 * unitem.get(l2)[0];
+									 */
+									boolean b = testitem(unitem.get(i)[0], unitem.get(j)[0], unitem.get(k)[0],
+											unitem.get(l)[0], unitem.get(l2)[0], unitem.get(m)[0]);
+									if (b) {
+										String[] add_f, add_f1, add2, val = lexicalFusion(unitem.get(i), unitem.get(j));
+										/*
+										 * String[] val3 = unitem.get(k);
+										 * String[] val4 = unitem.get(l);
+										 * String[] val5 = unitem.get(l2);
+										 * String[] add = Fusion(val, val3);
+										 * add_f = Fusion(add, val4); add_f1 =
+										 * Fusion(add_f, val5); add2 =
+										 * Fusion(add_f1, unitem.get(m));
+										 */
+										add2 = Fusion(Fusion(Fusion(
+												Fusion(lexicalFusion(unitem.get(i), unitem.get(j)), unitem.get(k)),
+												unitem.get(l)), unitem.get(l2)), unitem.get(m));
+
+										valrtn.add(add2);
+									}
+								}
+							}
+						}
+					}
+
+				}
+			}
+			break;
+
+		case 14:
+			for (int i = 0; i < 2; i++) {
+				for (int j = 2; j < 4; j++) {
+					for (int k = 4; k < 6; k++) {
+						for (int l = 6; l < 8; l++) {
+							for (int l2 = 8; l2 < 10; l2++) {
+								for (int m = 10; m < 12; m++) {
+									for (int m2 = 12; m2 < 14; m2++) {
+										boolean b = testitem(unitem.get(i)[0], unitem.get(j)[0], unitem.get(k)[0],
+												unitem.get(l)[0], unitem.get(l2)[0], unitem.get(m)[0],
+												unitem.get(m2)[0]);
+										if (b) {
+											String[] add_f;
+
+											add_f = Fusion(Fusion(
+													Fusion(Fusion(Fusion(lexicalFusion(unitem.get(i), unitem.get(j)),
+															unitem.get(k)), unitem.get(l)), unitem.get(l2)),
+													unitem.get(m)), unitem.get(m2));
+
+											valrtn.add(add_f);
+										}
+									}
+
+								}
+							}
+						}
+					}
+
+				}
+			}
+			break;
+
+		case 16:
+			for (int i = 0; i < 2; i++) {
+				for (int j = 2; j < 4; j++) {
+					for (int k = 4; k < 6; k++) {
+						for (int l = 6; l < 8; l++) {
+							for (int l2 = 8; l2 < 10; l2++) {
+								for (int m = 10; m < 12; m++) {
+									for (int m2 = 12; m2 < 14; m2++) {
+										for (int n = 14; n < 16; n++) {
+											boolean b = testitem(unitem.get(i)[0], unitem.get(j)[0], unitem.get(k)[0],
+													unitem.get(l)[0], unitem.get(l2)[0], unitem.get(m)[0],
+													unitem.get(m2)[0], unitem.get(n)[0]);
+											if (b) {
+												String[] add_f;
+
+												add_f = Fusion(Fusion(Fusion(
+														Fusion(Fusion(
+																Fusion(lexicalFusion(unitem.get(i), unitem.get(j)),
+																		unitem.get(k)),
+																unitem.get(l)), unitem.get(l2)),
+														unitem.get(m)), unitem.get(m2)), unitem.get(n));
+
+												valrtn.add(add_f);
+											}
+										}
+
+									}
+
+								}
+							}
+						}
+					}
+
+				}
+			}
+			break;
+
+		case 18:
+			for (int i = 0; i < 2; i++) {
+				for (int j = 2; j < 4; j++) {
+					for (int k = 4; k < 6; k++) {
+						for (int l = 6; l < 8; l++) {
+							for (int l2 = 8; l2 < 10; l2++) {
+								for (int m = 10; m < 12; m++) {
+									for (int m2 = 12; m2 < 14; m2++) {
+										for (int n = 14; n < 16; n++) {
+											for (int n3 = 16; n3 < 18; n3++) {
+												boolean b = testitem(unitem.get(i)[0], unitem.get(j)[0],
+														unitem.get(k)[0], unitem.get(l)[0], unitem.get(l2)[0],
+														unitem.get(m)[0], unitem.get(m2)[0], unitem.get(n)[0],
+														unitem.get(n3)[0]);
+												if (b) {
+													String[] add_f;
+
+													add_f = Fusion(
+															Fusion(Fusion(
+																	Fusion(Fusion(
+																			Fusion(Fusion(
+																					lexicalFusion(unitem.get(i),
+																							unitem.get(j)),
+																					unitem.get(k)), unitem.get(l)),
+																			unitem.get(l2)), unitem.get(m)),
+																	unitem.get(m2)), unitem.get(n)),
+															unitem.get(n3));
+
+													valrtn.add(add_f);
+												}
+											}
+										}
+
+									}
+
+								}
+							}
+						}
+					}
+
+				}
+			}
+			break;
+
+		case 20:
+			for (int i = 0; i < 2; i++) {
+				for (int j = 2; j < 4; j++) {
+					for (int k = 4; k < 6; k++) {
+						for (int l = 6; l < 8; l++) {
+							for (int l2 = 8; l2 < 10; l2++) {
+								for (int m = 10; m < 12; m++) {
+									for (int m2 = 12; m2 < 14; m2++) {
+										for (int n = 14; n < 16; n++) {
+											for (int n3 = 16; n3 < 18; n3++) {
+												for (int o = 18; o < 20; o++) {
+													boolean b = testitem(unitem.get(i)[0], unitem.get(j)[0],
+															unitem.get(k)[0], unitem.get(l)[0], unitem.get(l2)[0],
+															unitem.get(m)[0], unitem.get(m2)[0], unitem.get(n)[0],
+															unitem.get(n3)[0], unitem.get(o)[0]);
+													if (b) {
+														String[] add_f, add_fnxt;
+
+														add_f = Fusion(
+																Fusion(Fusion(
+																		Fusion(Fusion(
+																				Fusion(Fusion(
+																						lexicalFusion(unitem.get(i),
+																								unitem.get(j)),
+																						unitem.get(k)), unitem.get(l)),
+																				unitem.get(l2)), unitem.get(m)),
+																		unitem.get(m2)), unitem.get(n)),
+																unitem.get(n3));
+														add_fnxt = Fusion(add_f, unitem.get(o));
+
+														valrtn.add(add_fnxt);
+													}
+												}
+											}
+										}
+
+									}
+
+								}
+							}
+						}
+					}
+
+				}
+			}
+			break;
+
+		case 22:
+			for (int i = 0; i < 2; i++) {
+				for (int j = 2; j < 4; j++) {
+					for (int k = 4; k < 6; k++) {
+						for (int l = 6; l < 8; l++) {
+							for (int l2 = 8; l2 < 10; l2++) {
+								for (int m = 10; m < 12; m++) {
+									for (int m2 = 12; m2 < 14; m2++) {
+										for (int n = 14; n < 16; n++) {
+											for (int n3 = 16; n3 < 18; n3++) {
+												for (int o = 18; o < 20; o++) {
+													for (int o2 = 20; o2 < 22; o2++) {
+														boolean b = testitem(unitem.get(i)[0], unitem.get(j)[0],
+																unitem.get(k)[0], unitem.get(l)[0], unitem.get(l2)[0],
+																unitem.get(m)[0], unitem.get(m2)[0], unitem.get(n)[0],
+																unitem.get(n3)[0], unitem.get(o)[0], unitem.get(o2)[0]);
+														if (b) {
+															String[] add_f, add_fnxt;
+
+															add_f = Fusion(
+																	Fusion(Fusion(
+																			Fusion(Fusion(
+																					Fusion(Fusion(
+																							lexicalFusion(unitem.get(i),
+																									unitem.get(j)),
+																							unitem.get(k)),
+																							unitem.get(l)),
+																					unitem.get(l2)), unitem.get(m)),
+																			unitem.get(m2)), unitem.get(n)),
+																	unitem.get(n3));
+															add_fnxt = Fusion(Fusion(add_f, unitem.get(o)),
+																	unitem.get(o2));
+
+															valrtn.add(add_fnxt);
+														}
+													}
+												}
+											}
+										}
+
+									}
+
+								}
+							}
+						}
+					}
+
+				}
+			}
+			break;
+
+		default:
+			break;
+		}
+		return valrtn;
+
+	}
+
+	public boolean testitem(String... listItem) {
+		int taille = listItem.length;
+		boolean b = true;
+		String[] tabitem = new String[taille];
+		for (int i = 0; i < taille; i++) {
+			tabitem[i] = listItem[i];
+		}
+		for (int i = 0; i < tabitem.length; i++) {
+			for (int j = i + 1; j < tabitem.length; j++) {
+				b = b && !(tabitem[i] == tabitem[j]);
+			}
+		}
+		return b;
+
+	}
+
+	private String[] Fusion(String[] val, String[] val3) {
+		int j = 0, p = val.length, q = val3.length;
+		String[] r = new String[p + q];
+		for (j = 0; j < p; j++) {
+			r[j] = val[j];
+		}
+		for (int i = 0; i < val3.length; i++) {
+			r[j + i] = val3[i];
+		}
+		return r;
+	}
+
 }
